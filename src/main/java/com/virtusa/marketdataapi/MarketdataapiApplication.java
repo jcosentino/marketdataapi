7bd4870e3924f05ff3f20d2923dd48485c58d74a
jcosentino <johnsc301@gmail.com>
2020-06-22 16:31:32 -0400

    Added initial maven project

---
+++ src/main/java/com/virtusa/marketdataapi/MarketdataapiApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.marketdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketdataapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketdataapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtusa/marketdataapi/MarketdataapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.marketdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketdataapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
